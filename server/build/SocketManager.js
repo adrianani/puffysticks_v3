8d4b83e1e41cb4f2b88e261c3003a07ec47c80a8
Portocala <30348211+Portocala@users.noreply.github.com>
2019-12-23 16:03:03 +0200

    SocketManager added

---
+++ server/build/SocketManager.js
@@ -0,0 +1,56 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var mongoose_1 = require("./mongoose");
+exports.default = (function (socket, io) {
+    console.log(socket.id);
+    /* Lang.create({shortcut: 'en', name: 'english', default: true}, (err, lang) => {
+        console.log(lang.id, err);
+        db.LangWord.create(
+            {key: 'work_with_us_question', string: 'Interested in working with us?', langid: lang._id},
+            {key: 'contact_us', string: 'get in touch', langid: lang._id},
+            {key: 'categories', string: 'categories', langid: lang._id},
+            {key: 'all', string: 'all', langid: lang._id},
+            {key: 'logos', string: 'logos', langid: lang._id},
+            {key: 'web_design', string: 'web design', langid: lang._id},
+            {key: 'ipb_themes', string: 'ipb design', langid: lang._id},
+            {key: 'illustrations_and_drawings', string: 'illustration & drawings', langid: lang._id},
+        );
+    }); */
+    /**
+     * @desc fetch lang words from database on request
+     * @param object    data    - words to be returned
+     * @param function  cb      - callback to execute with response
+     *
+     * @return void
+     */
+    socket.on('get lang words', function (data, cb) {
+        mongoose_1.default.LangWord.find({
+            key: { $in: data.key },
+            langid: data.langid,
+            articleid: data.articleid,
+        }, {
+            '_id': 0,
+            'string': 1,
+            'key': 1
+        }, function (err, docs) {
+            var success = true, res = {}, errors = [];
+            if (err) {
+                success = false;
+                errors.push('Unexpected lang error');
+            }
+            if (docs.length == 0) {
+                success = false;
+                errors.push('Unknown lang key');
+            }
+            docs.forEach(function (value) {
+                res[value.key] = value.string;
+            });
+            cb({ success: success, res: res, errors: errors });
+        });
+    });
+    // Create lang words
+    socket.on('create lang words', function (data, cb) {
+        mongoose_1.default.Lang.create(data, function (err, docs) {
+        });
+    });
+});
